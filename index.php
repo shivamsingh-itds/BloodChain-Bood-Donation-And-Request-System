1aa392c7dc6b1afd1a1c8fdcaac1fbd48b41c831
shivamsingh-itds <shivam.itds@gmail.com>
2026-04-26 23:01:51 +0530

    index page

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blood Bank Management</title>
-    <!-- Bootstrap files -->
+ 
     <link rel="stylesheet" href="bootstrap/css//bootstrap.min.css">
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- External CSS file -->
+    
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
